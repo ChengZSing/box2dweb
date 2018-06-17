36d3569b993dac3b09a1475dade15515cfaae94b
ChengZSing <chzxing@gmail.com>
2018-06-17 15:22:41 +0800

    edit box2dweb for egret

--- Box2D.js
+++ Box2D.js
@@ -5986,7 +5986,9 @@ Box2D.postDefs = [];
       if (this.m_debugDraw == null) {
          return;
       }
-      this.m_debugDraw.m_sprite.graphics.clear();
+      // this.m_debugDraw.m_sprite.graphics.clear();
+      // edit for egret
+      this.m_debugDraw.m_ctx.graphics.clear();
       var flags = this.m_debugDraw.GetFlags();
       var i = 0;
       var b;
@@ -10783,41 +10785,62 @@ Box2D.postDefs = [];
       if (!vertexCount) return;
       var s = this.m_ctx;
       var drawScale = this.m_drawScale;
-      s.beginPath();
-      s.strokeStyle = this._color(color.color, this.m_alpha);
-      s.moveTo(vertices[0].x * drawScale, vertices[0].y * drawScale);
+      // s.beginPath();
+      // s.strokeStyle = this._color(color.color, this.m_alpha);
+      // s.moveTo(vertices[0].x * drawScale, vertices[0].y * drawScale);
+      // for (var i = 1; i < vertexCount; i++) {
+      //    s.lineTo(vertices[i].x * drawScale, vertices[i].y * drawScale);
+      // }
+      // s.lineTo(vertices[0].x * drawScale, vertices[0].y * drawScale);
+      // s.closePath();
+      // s.stroke();
+      // edit for egret
+      s.graphics.lineStyle(this.m_lineThickness,color.color,this.m_alpha);
+      s.graphics.moveTo(vertices[0].x * drawScale, vertices[0].y * drawScale);
       for (var i = 1; i < vertexCount; i++) {
-         s.lineTo(vertices[i].x * drawScale, vertices[i].y * drawScale);
+         s.graphics.lineTo(vertices[i].x * drawScale, vertices[i].y * drawScale);
       }
-      s.lineTo(vertices[0].x * drawScale, vertices[0].y * drawScale);
-      s.closePath();
-      s.stroke();
+      s.graphics.lineTo(vertices[0].x * drawScale, vertices[0].y * drawScale);
    };
    b2DebugDraw.prototype.DrawSolidPolygon = function (vertices, vertexCount, color) {
       if (!vertexCount) return;
       var s = this.m_ctx;
       var drawScale = this.m_drawScale;
-      s.beginPath();
-      s.strokeStyle = this._color(color.color, this.m_alpha);
-      s.fillStyle = this._color(color.color, this.m_fillAlpha);
-      s.moveTo(vertices[0].x * drawScale, vertices[0].y * drawScale);
+      // s.beginPath();
+      // s.strokeStyle = this._color(color.color, this.m_alpha);
+      // s.fillStyle = this._color(color.color, this.m_fillAlpha);
+      // s.moveTo(vertices[0].x * drawScale, vertices[0].y * drawScale);
+      // for (var i = 1; i < vertexCount; i++) {
+      //    s.lineTo(vertices[i].x * drawScale, vertices[i].y * drawScale);
+      // }
+      // s.lineTo(vertices[0].x * drawScale, vertices[0].y * drawScale);
+      // s.closePath();
+      // s.fill();
+      // s.stroke();
+      // edit for egret
+      s.graphics.lineStyle(this.m_lineThickness,color.color,this.m_alpha);
+      s.graphics.beginFill(color.color,this.m_fillAlpha);
+      s.graphics.moveTo(vertices[0].x * drawScale, vertices[0].y * drawScale);
       for (var i = 1; i < vertexCount; i++) {
-         s.lineTo(vertices[i].x * drawScale, vertices[i].y * drawScale);
+         s.graphics.lineTo(vertices[i].x * drawScale, vertices[i].y * drawScale);
       }
-      s.lineTo(vertices[0].x * drawScale, vertices[0].y * drawScale);
-      s.closePath();
-      s.fill();
-      s.stroke();
+      s.graphics.lineTo(vertices[0].x * drawScale, vertices[0].y * drawScale);
+      s.graphics.endFill();
    };
    b2DebugDraw.prototype.DrawCircle = function (center, radius, color) {
       if (!radius) return;
       var s = this.m_ctx;
       var drawScale = this.m_drawScale;
-      s.beginPath();
-      s.strokeStyle = this._color(color.color, this.m_alpha);
-      s.arc(center.x * drawScale, center.y * drawScale, radius * drawScale, 0, Math.PI * 2, true);
-      s.closePath();
-      s.stroke();
+      // s.beginPath();
+      // s.strokeStyle = this._color(color.color, this.m_alpha);
+      // s.arc(center.x * drawScale, center.y * drawScale, radius * drawScale, 0, Math.PI * 2, true);
+      // s.closePath();
+      // s.stroke();
+      // edit for egret
+      s.graphics.lineStyle(this.m_lineThickness,color.color,this.m_alpha);
+      s.graphics.drawCircle(center.x * drawScale, center.y * drawScale, radius * drawScale);
+      s.graphics.endFill();
+
    };
    b2DebugDraw.prototype.DrawSolidCircle = function (center, radius, axis, color) {
       if (!radius) return;
@@ -10825,40 +10848,62 @@ Box2D.postDefs = [];
          drawScale = this.m_drawScale,
          cx = center.x * drawScale,
          cy = center.y * drawScale;
-      s.moveTo(0, 0);
-      s.beginPath();
-      s.strokeStyle = this._color(color.color, this.m_alpha);
-      s.fillStyle = this._color(color.color, this.m_fillAlpha);
-      s.arc(cx, cy, radius * drawScale, 0, Math.PI * 2, true);
-      s.moveTo(cx, cy);
-      s.lineTo((center.x + axis.x * radius) * drawScale, (center.y + axis.y * radius) * drawScale);
-      s.closePath();
-      s.fill();
-      s.stroke();
+      // s.moveTo(0, 0);
+      // s.beginPath();
+      // s.strokeStyle = this._color(color.color, this.m_alpha);
+      // s.fillStyle = this._color(color.color, this.m_fillAlpha);
+      // s.arc(cx, cy, radius * drawScale, 0, Math.PI * 2, true);
+      // s.moveTo(cx, cy);
+      // s.lineTo((center.x + axis.x * radius) * drawScale, (center.y + axis.y * radius) * drawScale);
+      // s.closePath();
+      // s.fill();
+      // s.stroke();
+      // edit for egret
+      s.graphics.moveTo(0,0);
+      s.graphics.lineStyle(this.m_lineThickness,color.color,this.m_alpha);
+      s.graphics.beginFill(color.color,this.m_fillAlpha);
+      s.graphics.drawCircle(center.x * drawScale, center.y * drawScale, radius * drawScale);
+      s.graphics.endFill();
+
+      s.graphics.moveTo(0,0);
+      s.graphics.lineTo((center.x + axis.x * radius) * drawScale, (center.y + axis.y * radius) * drawScale);
+      
    };
    b2DebugDraw.prototype.DrawSegment = function (p1, p2, color) {
       var s = this.m_ctx,
          drawScale = this.m_drawScale;
-      s.strokeStyle = this._color(color.color, this.m_alpha);
-      s.beginPath();
-      s.moveTo(p1.x * drawScale, p1.y * drawScale);
-      s.lineTo(p2.x * drawScale, p2.y * drawScale);
-      s.closePath();
-      s.stroke();
+      // s.strokeStyle = this._color(color.color, this.m_alpha);
+      // s.beginPath();
+      // s.moveTo(p1.x * drawScale, p1.y * drawScale);
+      // s.lineTo(p2.x * drawScale, p2.y * drawScale);
+      // s.closePath();
+      // s.stroke();
+      // edit for egret
+      s.graphics.lineStyle(this.m_lineThickness,color.color,this.m_alpha);
+      s.graphics.moveTo(p1.x * drawScale, p1.y * drawScale);
+      s.graphics.lineTo(p2.x * drawScale, p2.y * drawScale);
    };
    b2DebugDraw.prototype.DrawTransform = function (xf) {
       var s = this.m_ctx,
          drawScale = this.m_drawScale;
-      s.beginPath();
-      s.strokeStyle = this._color(0xff0000, this.m_alpha);
-      s.moveTo(xf.position.x * drawScale, xf.position.y * drawScale);
-      s.lineTo((xf.position.x + this.m_xformScale * xf.R.col1.x) * drawScale, (xf.position.y + this.m_xformScale * xf.R.col1.y) * drawScale);
+      // s.beginPath();
+      // s.strokeStyle = this._color(0xff0000, this.m_alpha);
+      // s.moveTo(xf.position.x * drawScale, xf.position.y * drawScale);
+      // s.lineTo((xf.position.x + this.m_xformScale * xf.R.col1.x) * drawScale, (xf.position.y + this.m_xformScale * xf.R.col1.y) * drawScale);
 
-      s.strokeStyle = this._color(0xff00, this.m_alpha);
-      s.moveTo(xf.position.x * drawScale, xf.position.y * drawScale);
-      s.lineTo((xf.position.x + this.m_xformScale * xf.R.col2.x) * drawScale, (xf.position.y + this.m_xformScale * xf.R.col2.y) * drawScale);
-      s.closePath();
-      s.stroke();
+      // s.strokeStyle = this._color(0xff00, this.m_alpha);
+      // s.moveTo(xf.position.x * drawScale, xf.position.y * drawScale);
+      // s.lineTo((xf.position.x + this.m_xformScale * xf.R.col2.x) * drawScale, (xf.position.y + this.m_xformScale * xf.R.col2.y) * drawScale);
+      // s.closePath();
+      // s.stroke();
+      // edit for egret
+      s.graphics.lineStyle(this.m_lineThickness,0xff0000,this.m_alpha)
+      s.graphics.moveTo(xf.position.x * drawScale, xf.position.y * drawScale);
+      s.graphics.lineTo((xf.position.x + this.m_xformScale * xf.R.col1.x) * drawScale, (xf.position.y + this.m_xformScale * xf.R.col1.y) * drawScale);
+
+      s.graphics.lineStyle(this.m_lineThickness,0xff00,this.m_alpha);
+      s.graphics.moveTo(xf.position.x * drawScale, xf.position.y * drawScale);
+      s.graphics.lineTo((xf.position.x + this.m_xformScale * xf.R.col2.x) * drawScale, (xf.position.y + this.m_xformScale * xf.R.col2.y) * drawScale);
    };
 })(); //post-definitions
 var i;
